db3a5b020cac6911bead39e358e869d97c96f251
colincrist <colincrist@6ec42ede-3d11-0410-a3df-d4e4f9070504>
2013-01-08 11:16:45 +0000

    [minor] domain handling for topics was incorrect
    git-svn-id: https://hermesjms.svn.sourceforge.net/svnroot/hermesjms/trunk/HermesJMS@127 6ec42ede-3d11-0410-a3df-d4e4f9070504

--- src/java/hermes/ext/imq/ImqAdmin.java
+++ src/java/hermes/ext/imq/ImqAdmin.java
@@ -53,278 +53,234 @@ import org.apache.commons.logging.LogFactory;
  * @version $Revision$
  * @since 01.00.00.00
  */
-public class ImqAdmin extends HermesAdminSupport implements HermesAdmin, MessageListener
-{
-   static final String DEST_LIST_TOPIC_NAME = "mq.metrics.destination_list";
-   static final String QUEUE_METRICS_TOPIC_PREFIX = "mq.metrics.destination.queue.";
-   static final String TOPIC_METRICS_TOPIC_PREFIX = "mq.metrics.destination.topic.";
+public class ImqAdmin extends HermesAdminSupport implements HermesAdmin, MessageListener {
+	static final String DEST_LIST_TOPIC_NAME = "mq.metrics.destination_list";
+	static final String QUEUE_METRICS_TOPIC_PREFIX = "mq.metrics.destination.queue.";
+	static final String TOPIC_METRICS_TOPIC_PREFIX = "mq.metrics.destination.topic.";
 
-   private ConnectionFactory connectionFactory;
-   private Connection connection;
-   private Session session;
-   private MessageConsumer destListTopicSubscriber;
+	private final ConnectionFactory connectionFactory;
+	private Connection connection;
+	private Session session;
+	private MessageConsumer destListTopicSubscriber;
 
-   private Map<String, MessageConsumer> destMetricTopicSubscribers = new HashMap<String, MessageConsumer>();
-   private Map<String, Long> messageCounts = new HashMap<String, Long>();
-   private Map<String, Map<String, Long>> stats = new HashMap<String, Map<String, Long>>();
+	private final Map<String, MessageConsumer> destMetricTopicSubscribers = new HashMap<String, MessageConsumer>();
+	private final Map<String, Long> messageCounts = new HashMap<String, Long>();
+	private final Map<String, Map<String, Long>> stats = new HashMap<String, Map<String, Long>>();
 
-   private List<DestinationConfig> destinations = new ArrayList<DestinationConfig>();
+	private List<DestinationConfig> destinations = new ArrayList<DestinationConfig>();
 
-   private static Log LOG = LogFactory.getLog(ImqAdminFactory.class);
+	private static Log LOG = LogFactory.getLog(ImqAdminFactory.class);
 
-   private Object destListGuard = new Object();
-   private Object destMetricGuard = new Object();
+	private final Object destListGuard = new Object();
+	private final Object destMetricGuard = new Object();
 
-   public ImqAdmin(Hermes hermes, ConnectionFactory connectionFactory)
-   {
-      super(hermes);
-      this.connectionFactory = connectionFactory;
+	public ImqAdmin(Hermes hermes, ConnectionFactory connectionFactory) {
+		super(hermes);
+		this.connectionFactory = connectionFactory;
 
-      LOG.debug("Building ImqAdmin");
+		LOG.debug("Building ImqAdmin");
 
-      try
-      {
-         connection = this.connectionFactory.createConnection();
-         connection.start();
-         session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-         Topic destListTopic = session.createTopic(DEST_LIST_TOPIC_NAME);
-         destListTopicSubscriber = session.createConsumer(destListTopic);
-         destListTopicSubscriber.setMessageListener(this);
-      }
-      catch (JMSException e)
-      {
-         throw new RuntimeException(e);
-      }
-   }
+		try {
+			connection = this.connectionFactory.createConnection();
+			connection.start();
+			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			Topic destListTopic = session.createTopic(DEST_LIST_TOPIC_NAME);
+			destListTopicSubscriber = session.createConsumer(destListTopic);
+			destListTopicSubscriber.setMessageListener(this);
+		} catch (JMSException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-   @Override
-   public Collection<DestinationConfig> discoverDestinationConfigs() throws JMSException
-   {
+	@Override
+	public Collection<DestinationConfig> discoverDestinationConfigs() throws JMSException {
 
-      LOG.debug("Discovering destination(s)");
+		LOG.debug("Discovering destination(s)");
 
-      try
-      {
-         synchronized (destListGuard)
-         {
-            destListGuard.wait();
-         }
-      }
-      catch (InterruptedException e)
-      {
-      }
+		try {
+			synchronized (destListGuard) {
+				destListGuard.wait();
+			}
+		} catch (InterruptedException e) {
+		}
 
-      LOG.debug("Discovered " + destinations.size() + " destination(s)");
+		LOG.debug("Discovered " + destinations.size() + " destination(s)");
 
-      return destinations;
-   }
+		return destinations;
+	}
 
-   @Override
-   public int getDepth(DestinationConfig destinationConfig) throws JMSException
-   {
-      LOG.debug("Getting depth");
+	@Override
+	public int getDepth(DestinationConfig destinationConfig) throws JMSException {
+		LOG.debug("Getting depth");
 
-      try
-      {
-         subscribeToDestMetricTopic(destinationConfig.getShortName(), destinationConfig.getDomain());
+		try {
+			subscribeToDestMetricTopic(destinationConfig.getShortName(), destinationConfig.getDomain());
 
-         synchronized (destMetricGuard)
-         {
-            destMetricGuard.wait(5000);
-         }
-      }
-      catch (InterruptedException e)
-      {
-      }
+			synchronized (destMetricGuard) {
+				destMetricGuard.wait(5000);
+			}
+		} catch (InterruptedException e) {
+		}
 
-      Long result = messageCounts.get(destinationConfig.getShortName());
+		Long result = messageCounts.get(destinationConfig.getShortName());
 
-      LOG.debug("Got depth for " + destinationConfig.getShortName() + ": " + result);
+		LOG.debug("Got depth for " + destinationConfig.getShortName() + ": " + result);
 
-      if (result == null)
-      {
-         throw new RuntimeException("Timeout: Got no data from metric topic.");
-      }
+		if (result == null) {
+			throw new RuntimeException("Timeout: Got no data from metric topic.");
+		}
 
-      clearDestMetricTopicSubscribers();
+		clearDestMetricTopicSubscribers();
 
-      return result.intValue();
-   }
+		return result.intValue();
+	}
 
-   @Override
-   public Map<String, Long> getStatistics(DestinationConfig destination) throws JMSException
-   {
-      LOG.debug("Getting statistics for " + destination);
+	@Override
+	public Map<String, Long> getStatistics(DestinationConfig destination) throws JMSException {
+		LOG.debug("Getting statistics for " + destination);
 
-      try
-      {
-         synchronized (destMetricGuard)
-         {
-            subscribeToDestMetricTopic(destination.getShortName(), destination.getDomain());
+		try {
+			synchronized (destMetricGuard) {
+				subscribeToDestMetricTopic(destination.getShortName(), destination.getDomain());
 
-            destMetricGuard.wait(5000);
-         }
-      }
-      catch (InterruptedException e)
-      {
-      }
+				destMetricGuard.wait(5000);
+			}
+		} catch (InterruptedException e) {
+		}
 
-      Map<String, Long> result = stats.get(destination.getShortName());
+		Map<String, Long> result = stats.get(destination.getShortName());
 
-      clearDestMetricTopicSubscribers();
+		clearDestMetricTopicSubscribers();
 
-      if (result == null)
-      {
-         throw new RuntimeException("Timeout: Got no data from metric topic.");
-      }
+		if (result == null) {
+			throw new RuntimeException("Timeout: Got no data from metric topic.");
+		}
 
-      return result;
-   }
+		return result;
+	}
 
-   public void close() throws JMSException
-   {
-      LOG.debug("Closing IMQ Session");
-      clearDestMetricTopicSubscribers();
-      destListTopicSubscriber.close();
-      session.close();
-      connection.close();
-   }
+	@Override
+	public void close() throws JMSException {
+		LOG.debug("Closing IMQ Session");
+		clearDestMetricTopicSubscribers();
+		destListTopicSubscriber.close();
+		session.close();
+		connection.close();
+	}
 
-   public void onMessage(Message msg)
-   {
+	@Override
+	public void onMessage(Message msg) {
 
-      try
-      {
+		try {
 
-         MapMessage mapMsg = (MapMessage) msg;
-         String type = mapMsg.getStringProperty("type");
+			MapMessage mapMsg = (MapMessage) msg;
+			String type = mapMsg.getStringProperty("type");
 
-         LOG.debug("Got admin message from broker of type: " + type);
+			LOG.debug("Got admin message from broker of type: " + type);
 
-         if (type.equals(DEST_LIST_TOPIC_NAME))
-         {
-            List<DestinationConfig> result = new ArrayList<DestinationConfig>();
+			if (type.equals(DEST_LIST_TOPIC_NAME)) {
+				List<DestinationConfig> result = new ArrayList<DestinationConfig>();
 
-            for (@SuppressWarnings("unchecked")
-            Enumeration e = mapMsg.getMapNames(); e.hasMoreElements();)
-            {
-               String name = (String) e.nextElement();
+				for (@SuppressWarnings("unchecked")
+				Enumeration e = mapMsg.getMapNames(); e.hasMoreElements();) {
+					String name = (String) e.nextElement();
 
-               @SuppressWarnings("unchecked")
-               Map<String, String> object = (Map<String, String>) mapMsg.getObject(name);
+					@SuppressWarnings("unchecked")
+					Map<String, String> object = (Map<String, String>) mapMsg.getObject(name);
 
-               DestinationConfig dest = HermesBrowser.getConfigDAO().createDestinationConfig();
-               dest.setName(object.get("name"));
-               dest.setShortName(object.get("name"));
-               dest.setDomain("queue".equals(object.get("type")) ? Domain.QUEUE.getId() : ("queue".equals(object.get("topic")) ? Domain.TOPIC.getId()
-                     : Domain.UNKNOWN.getId()));
+					DestinationConfig dest = HermesBrowser.getConfigDAO().createDestinationConfig();
+					dest.setName(object.get("name"));
+					dest.setShortName(object.get("name"));
+					dest.setDomain("queue".equals(object.get("type")) ? Domain.QUEUE.getId() : ("topic".equals(object.get("type")) ? Domain.TOPIC.getId() : Domain.UNKNOWN.getId()));
 
-               result.add(dest);
-            }
+					result.add(dest);
+				}
 
-            Collections.sort(result, new Comparator<DestinationConfig>()
-            {
-               public int compare(DestinationConfig o1, DestinationConfig o2)
-               {
-                  return o1.getShortName().compareTo(o2.getShortName());
-               }
-            });
+				Collections.sort(result, new Comparator<DestinationConfig>() {
+					@Override
+					public int compare(DestinationConfig o1, DestinationConfig o2) {
+						return o1.getShortName().compareTo(o2.getShortName());
+					}
+				});
 
-            destinations = result;
+				destinations = result;
 
-            synchronized (destListGuard)
-            {
-               destListGuard.notifyAll();
-            }
+				synchronized (destListGuard) {
+					destListGuard.notifyAll();
+				}
 
-         }
-         else if (type.startsWith(QUEUE_METRICS_TOPIC_PREFIX))
-         {
-            LOG.debug("Got queue metrics: " + type);
+			} else if (type.startsWith(QUEUE_METRICS_TOPIC_PREFIX)) {
+				LOG.debug("Got queue metrics: " + type);
 
-            String queueName = type.substring(QUEUE_METRICS_TOPIC_PREFIX.length());
-            messageCounts.put(queueName, mapMsg.getLong("numMsgs"));
-            HashMap<String, Long> map = new HashMap<String, Long>();
+				String queueName = type.substring(QUEUE_METRICS_TOPIC_PREFIX.length());
+				messageCounts.put(queueName, mapMsg.getLong("numMsgs"));
+				HashMap<String, Long> map = new HashMap<String, Long>();
 
-            @SuppressWarnings("unchecked")
-            Enumeration<String> e = mapMsg.getMapNames();
-            while (e.hasMoreElements())
-            {
-               String name = (String) e.nextElement();
-               map.put(name, mapMsg.getLong(name));
-            }
-            stats.put(queueName, map);
-            LOG.debug("Stored stats for: " + queueName);
+				@SuppressWarnings("unchecked")
+				Enumeration<String> e = mapMsg.getMapNames();
+				while (e.hasMoreElements()) {
+					String name = e.nextElement();
+					map.put(name, mapMsg.getLong(name));
+				}
+				stats.put(queueName, map);
+				LOG.debug("Stored stats for: " + queueName);
 
-            synchronized (destMetricGuard)
-            {
-               destMetricGuard.notifyAll();
-            }
+				synchronized (destMetricGuard) {
+					destMetricGuard.notifyAll();
+				}
 
-         }
-         else if (type.startsWith(TOPIC_METRICS_TOPIC_PREFIX))
-         {
-            LOG.debug("Got topic metrics: " + type);
+			} else if (type.startsWith(TOPIC_METRICS_TOPIC_PREFIX)) {
+				LOG.debug("Got topic metrics: " + type);
 
-            String topicName = type.substring(TOPIC_METRICS_TOPIC_PREFIX.length());
-            messageCounts.put(topicName, mapMsg.getLong("numMsgs"));
+				String topicName = type.substring(TOPIC_METRICS_TOPIC_PREFIX.length());
+				messageCounts.put(topicName, mapMsg.getLong("numMsgs"));
 
-            HashMap<String, Long> map = new HashMap<String, Long>();
+				HashMap<String, Long> map = new HashMap<String, Long>();
 
-            @SuppressWarnings("unchecked")
-            Enumeration<String> e = mapMsg.getMapNames();
-            while (e.hasMoreElements())
-            {
-               String name = (String) e.nextElement();
-               map.put(name, mapMsg.getLong(name));
-            }
-            stats.put(topicName, map);
-            LOG.debug("Stored stats for: " + topicName);
+				@SuppressWarnings("unchecked")
+				Enumeration<String> e = mapMsg.getMapNames();
+				while (e.hasMoreElements()) {
+					String name = e.nextElement();
+					map.put(name, mapMsg.getLong(name));
+				}
+				stats.put(topicName, map);
+				LOG.debug("Stored stats for: " + topicName);
 
-            synchronized (destMetricGuard)
-            {
-               destMetricGuard.notifyAll();
-            }
-         }
+				synchronized (destMetricGuard) {
+					destMetricGuard.notifyAll();
+				}
+			}
 
-      }
-      catch (JMSException e)
-      {
-         throw new RuntimeException(e);
-      }
-   }
+		} catch (JMSException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-   private MessageConsumer subscribeToDestMetricTopic(String destName, int domain) throws JMSException
-   {
-      LOG.debug("Subscribing to " + destName);
+	private MessageConsumer subscribeToDestMetricTopic(String destName, int domain) throws JMSException {
+		LOG.debug("Subscribing to " + destName);
 
-      String topicName = null;
+		String topicName = null;
 
-      if (domain == Domain.QUEUE.getId())
-      {
-         topicName = QUEUE_METRICS_TOPIC_PREFIX + destName;
-      }
-      else if (domain == Domain.TOPIC.getId())
-      {
-         topicName = TOPIC_METRICS_TOPIC_PREFIX + destName;
-      }
+		if (domain == Domain.QUEUE.getId()) {
+			topicName = QUEUE_METRICS_TOPIC_PREFIX + destName;
+		} else if (domain == Domain.TOPIC.getId()) {
+			topicName = TOPIC_METRICS_TOPIC_PREFIX + destName;
+		}
 
-      Topic destListTopic = session.createTopic(topicName);
-      MessageConsumer subscriber = session.createConsumer(destListTopic);
-      subscriber.setMessageListener(this);
+		Topic destListTopic = session.createTopic(topicName);
+		MessageConsumer subscriber = session.createConsumer(destListTopic);
+		subscriber.setMessageListener(this);
 
-      LOG.debug("Created subscriber " + subscriber + " listening to " + topicName);
-      return subscriber;
-   }
+		LOG.debug("Created subscriber " + subscriber + " listening to " + topicName);
+		return subscriber;
+	}
 
-   private void clearDestMetricTopicSubscribers() throws JMSException
-   {
-      for (MessageConsumer subscriber : destMetricTopicSubscribers.values())
-      {
-         LOG.debug("Closing subscriber: " + subscriber);
-         subscriber.close();
-      }
-      destMetricTopicSubscribers.clear();
-   }
+	private void clearDestMetricTopicSubscribers() throws JMSException {
+		for (MessageConsumer subscriber : destMetricTopicSubscribers.values()) {
+			LOG.debug("Closing subscriber: " + subscriber);
+			subscriber.close();
+		}
+		destMetricTopicSubscribers.clear();
+	}
 
 }
